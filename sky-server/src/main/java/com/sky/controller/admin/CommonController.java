0a3c8bce92b1ed53649555736ac37bc4212051f6
yehen <987893621@qq.com>
2026-04-21 18:56:58 +0800

    店铺营业状态设置模块功能代码

---
+++ sky-server/src/main/java/com/sky/controller/admin/CommonController.java
@@ -0,0 +1,20 @@
+package com.sky.controller.admin;
+
+import com.sky.result.Result;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@RequestMapping("/admin/common")
+@Api(tags = "通用接口")
+public class CommonController {
+    @ApiOperation("文件上传")
+    @PostMapping("/upload")
+    public Result<String> upload(MultipartFile file){
+        return null;
+    }
+}
